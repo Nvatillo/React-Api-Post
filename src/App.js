70bd57283f5087e0b52ed698817aa3662e829c5a
Francisco Gajardo <franciscogajardo.perez@gmail.com>
2021-10-07 19:02:16 -0300

    React Post method con ASP.Net core V2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = (props) => {
+
+  const [dataFormulario, setDataFormulario] = useState({
+    titulo:'',
+    descripcion:'',
+    fechalanzamiento:'',
+    autor:'',
+    
+  })
+
+  const handleInputChange = (event) =>{
+    //console.log(event.target.value)
+    setDataFormulario({
+        ...dataFormulario,
+        [event.target.name] : event.target.value
+    })
+}
+
+  const enviarData = async event =>{
+    event.preventDefault();
+    try {
+      let config = {
+        method:'POST',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin':'*'
+        },
+        body:JSON.stringify(dataFormulario)
+      } 
+     
+      let res = await fetch('https://localhost:44377/WeatherForecast/add',config)
+      let json = await res.json()
+
+      console.log(json)
+    }catch (error) {
+      
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="container mt-5" onSubmit={enviarData}>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label className="mt-2">Titulo</Label>
+            <Input className="mt-2" type="text"  name="titulo" placeholder="Titulo Libro" onChange={handleInputChange} />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label className="mt-2">Descripcion</Label>
+            <Input className="mt-2" type="text" name="descripcion"  placeholder="Descripcion Libro" onChange={handleInputChange} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row form >
+        <Col md={6}>
+        <FormGroup>
+          <Label className="mt-2">Fecha Lanzamiento</Label>
+          <Input className="mt-2"  type="datetime-local" name="fechalanzamiento"  placeholder="Fecha Lanzamiento Libro" onChange={handleInputChange} />
+        </FormGroup>
+        </Col>
+        <Col md={6}>
+        <FormGroup>
+          <Label className="mt-2">Autor</Label>
+          <Input className="mt-2" type="text" name="autor" placeholder="Autor Libro" onChange={handleInputChange} />
+        </FormGroup>
+        </Col>
+        <FormGroup>
+          <Col md={6}>
+          <Label className="mt-2">Precio</Label>
+          <Input className="mt-2" type="text" name="precio" placeholder="precio Libro" onChange={handleInputChange} />
+          </Col>
+        </FormGroup>
+      </Row>
+      <Button className="mt-4 btn btn-success" type="submit">Agregar Libro</Button>
+    </Form>
   );
 }
 
